8e15c313b67a8760481367bdc031a431920bd129
pranav157 <pranavgupta.v@gmail.com>
2021-08-06 08:10:43 +0200

    blurryLoading

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ const loadText = document.querySelector(".loading-text");
 
 let load = 0;
 
-let int = setInterval(blurring, 30);
+let int = setInterval(blurring, 50);
 
 function blurring() {
   load++;
   if (load > 99) {
     clearInterval(int);
   }
-  loadText.innerText = "${load}%";
+
+  loadText.innerText = `${load}%`;
   loadText.style.opacity = scale(load, 0, 100, 1, 0);
-  bg.style.filter = "blur(${scale(load, 0, 100, 30, 0)}px)";
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
